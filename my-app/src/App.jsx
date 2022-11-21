b2886f58b3618be8a5f80d71bb32aa5896d14a68
Lazur Niko <lazur.niko@gmail.com>
2022-11-21 15:29:34 +0200

    added some features

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from './Header';
 import Footer from "./Footer";
+import './App.scss';
 
+const cssClasses = "app app--active";
+const styleObj ={
+    fontStyle: "italic",
+};
 const data = <ul><li>1</li></ul>;
 const App = () => (
-  <div>
+  <div className={cssClasses} style={styleObj}>
 <h1>Hello123</h1>
 <Header />
 Some text
